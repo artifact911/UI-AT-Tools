085f5e839ea88dd69c2a660dac9559c6b0233fca
artifact911 <artifact911@gmail.com>
2022-10-17 12:29:55 +0300

    grid started

--- selenium-grid/src/test/java/org/art/SeleniumGridDemoTest.java
+++ selenium-grid/src/test/java/org/art/SeleniumGridDemoTest.java
@@ -1,20 +1,38 @@
 package org.art;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SeleniumGridDemoTest {
 
     @Test
-    public void googleSearch() {
+    public void googleSearch() throws InterruptedException, MalformedURLException {
 
-        System.setProperty("webdriver.chrome.driver",
-                           "/Users/anduser/Documents/_Projects/fromLessons/at-Tools-java/selenium-grid/grid/driver/chromedriver");
+//        System.setProperty("webdriver.chrome.driver",
+//                           "/Users/anduser/Documents/_Projects/fromLessons/at-Tools-java/selenium-grid/grid/driver/chromedriver");
+//
+//        WebDriver driver = new ChromeDriver();
 
-        WebDriver driver = new ChromeDriver();
+        // для запуска на Grid:
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("safari");
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.111.214:4444"), caps);
 
         driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("Selenium Grid 4");
+        driver.findElement(By.name("q")).submit();
+
+        Thread.sleep(3000);
+
+        driver.quit();
     }
 
 }
